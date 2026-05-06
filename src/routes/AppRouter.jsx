a723e782f4c76762407eaf7a167f5825bb660709
Veliz1232 <cr.veliz@duocuc.cl>
2026-05-06 16:16:58 -0400

    feat: implementar componentes UI compartidos y diseño base

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,17 +2,18 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Dashboard } from '../modules/dashboard/Dashboard';
 import { MapView } from '../modules/maps/MapView';
 import { ReportForm } from '../modules/reports/ReportForm';
+import { Navbar } from '../components/Navbar/Navbar';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-      {/* Aquí irá el Navbar genérico más adelante (Fase 3) */}
+      <Navbar />
+      
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/mapa" element={<MapView />} />
         <Route path="/reportar" element={<ReportForm />} />
         
-        {/* Ruta por defecto: Redirige al dashboard si entran a la raíz '/' */}
         <Route path="/*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
